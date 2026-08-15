22686362d8d8778f0662b172cd4ab47fe7024740
Punam <thokarpunam14@gmail.com>
2026-08-15 19:01:59 +0545

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Author from "./pages/Author";
+import Book from "./pages/Book";
+import Issuing from "./pages/Issuing";
+import Student from "./pages/Student";
+import Transaction from "./pages/Transaction";
+import Settings from "./pages/Settings";
 import AuthRoute from "./components/AuthRoute";
 import "./index.css";
 
@@ -20,6 +25,11 @@ function App() {
         {/* Protected Routes */}
         <Route element={<AuthRoute type="protected" />}>
           <Route path="/author" element={<Author />} />
+          <Route path="/books" element={<Book />} />
+          <Route path="/students" element={<Student />} />
+          <Route path="/issuing" element={<Issuing />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/setting" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
